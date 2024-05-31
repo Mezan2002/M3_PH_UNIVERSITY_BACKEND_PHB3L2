110fc6d848499dc9d32484b16790918d2e613915
mezan2002 <mezanurr009@gmail.com>
2024-05-31 19:35:10 +0600

    middleware added for validate requests

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,14 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { createStudentValidationSchema } from '../student/student.validation';
 import { UserControllers } from './user.controller';
-
-const shenaBahini = (req: Request, res: Response, next: NextFunction) => {
-  console.log('Hello world!');
-  next();
-};
 
 const router = express.Router();
 
-router.post('/create-student', shenaBahini, UserControllers.createStudent);
+router.post(
+  '/create-student',
+  validateRequest(createStudentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
