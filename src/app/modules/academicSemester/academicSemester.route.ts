943b8d12f36d160a7dd2b0626783a33fa9016787
mezan2002 <mezanurr009@gmail.com>
2024-06-01 09:11:18 +0600

    academic semester created successfully

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterControllers } from './academicSemester.controller';
+import { AcademicSemesterValidations } from './academicSemester.validation';
+
+const router = express.Router();
+
+// create academic semester
+router.post(
+  '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.createAcademicSemester,
+);
+
+export const AcademicSemesterRoutes = router;
