de35900d1085735c0ce6acb6fd4f3233d9a39e15
mezan2002 <mezanurr009@gmail.com>
2024-06-03 17:12:49 +0600

    academic faculty creating started

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
+
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
---
+++ src/app/modules/academicFaculty/name.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+class name {
+  constructor(parameters) {}
+}
